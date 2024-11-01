17cae653c85aba0bb667fcbe67b00154f78043f4
janithrijayodya <janithrijayodya@gmail.com>
2024-11-01 22:13:27 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomerHeaderComponent } from './pages/customer-header/customer-header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CustomerHeaderComponent,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/customer-header/customer-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-header.component.html',
+  styleUrl: './customer-header.component.css'
+})
+export class CustomerHeaderComponent {
+
+}
