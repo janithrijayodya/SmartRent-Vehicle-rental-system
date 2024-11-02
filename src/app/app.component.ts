63db22b05883f771787c16c513e909112f4f60c1
janithrijayodya <janithrijayodya@gmail.com>
2024-11-02 22:45:13 +0530

    completed about page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomerHeaderComponent } from './pages/customer-header/customer-header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CustomerHeaderComponent,HomeComponent],
+  imports: [RouterOutlet,CustomerHeaderComponent,HomeComponent,AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CustomerHeaderComponent } from '../customer-header/customer-header.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CustomerHeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
