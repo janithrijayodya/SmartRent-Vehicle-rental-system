fef29c0da9766aa042f16cc3d09f859366b5c675
janithrijayodya <janithrijayodya@gmail.com>
2024-11-09 12:44:50 +0530

    completed customer TS

--- src/app/admin/customer_manage/customer.component.ts
+++ src/app/admin/customer_manage/customer.component.ts
@@ -1,14 +1,94 @@
 import { Component } from '@angular/core';
 import { AdminHomeComponent } from '../admin-home/admin-home.component';
 import { AdminHeaderComponent } from '../admin-header/admin-header.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [AdminHeaderComponent],
+  imports: [AdminHeaderComponent,FormsModule,CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+  
+  constructor(private http:HttpClient){
+    this.loadBranches();
+    this.getAllCustomers();
+  }
 
+  // ===========LOAD BRANCHES===========
+
+  public branchList:any= [];
+
+  public loadBranches(){
+    this.http.get("http://localhost:8080/branch/get_all").subscribe(data=>{
+      this.branchList=data;
+    })
+  }
+
+  // =======ADD CUSTOMER======
+
+  public customer:any ={
+    DLNumber : "",
+    customerName : "",
+    customerContact : "",
+    customerEmail : "",
+    customerAddress : ""
+  }
+
+  public addCustomer(){
+    this.http.post(" http://localhost:8080/customer/add_customer", this.customer).subscribe(data=>{
+      alert("Customerr has benn added!");
+      this.getAllCustomers();
+    })
+  }
+
+  // ============ GET ALL CUSTOMERS===========
+
+  public customerList : any =[];
+
+  public getAllCustomers(){
+    this.http.get(" http://localhost:8080/customer/get_all").subscribe(data=>{
+      this.customerList=data;
+    })
+  }
+
+  // ============= DELETE CUSTOMER==============
+
+  public deleteCustomerByID(customerID:any){
+    this.http.delete(`http://localhost:8080/customer/delete_by_id/${customerID}`).subscribe(data=>{
+      alert("Customer has been deleted !")
+      this.getAllCustomers();
+    })
+  }
+
+  // ============= UPDATE CUSTOMER===============
+
+  public viewCustomer:any = {}
+
+  public viewUpdateModel(customer:any){
+    this.viewCustomer=customer;
+  }
+
+  public updateCustomer(){
+    this.http.put(" http://localhost:8080/customer/update_customer",this.viewCustomer).subscribe(data=>{
+      alert("Customer has been updated !");
+      this.getAllCustomers();
+    })
+  }
+
+  // =============SEARCH CUSTOMER BY DLNUMBER============
+
+  public DLicenseNumber:any;
+
+  public searchedCustomer:any ={};
+
+  public searchCustomerByDLNumber(){
+    this.http.get(`http://localhost:8080/customer/search_customer/${this.DLicenseNumber}`).subscribe(data=>{
+        this.searchedCustomer=data;
+    })
+  }
 }
--- src/app/admin/employee-manage/employee-manage.component.ts
+++ src/app/admin/employee-manage/employee-manage.component.ts
@@ -1,13 +1,61 @@
 import { Component } from '@angular/core';
 import { AdminHeaderComponent } from '../admin-header/admin-header.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-employee-manage',
   standalone: true,
-  imports: [AdminHeaderComponent],
+  imports: [AdminHeaderComponent,CommonModule,FormsModule],
   templateUrl: './employee-manage.component.html',
   styleUrl: './employee-manage.component.css'
 })
 export class EmployeeManageComponent {
+
+  constructor(private http:HttpClient){
+    this.loadBranches();
+  }
+
+  // ===========LOAD BRANCHES===========
+
+  public branchList:any= [];
+
+  public loadBranches(){
+    this.http.get("http://localhost:8080/branch/get_all").subscribe(data=>{
+      this.branchList=data;
+    })
+  }
+
+  // =============ADD EMPLOYEE==========
+
+  public employee:any ={
+    nic : "",
+    employeeName : "",
+    employeeAddress : "",
+    employeeContact : "",
+    employeePosition : "",
+    branchID : ""
+  };
+
+  public addEmployee(){
+    this.http.post(" http://localhost:8080/employee/add_employee",this.employee).subscribe(data=>{
+      alert("Employee has been added !");
+    })
+  }
+
+  // ========== DELETE EMPLOYEE ==========
+
+  public deleteEmployee(){
+    // this.http.delete(" http://localhost:8080/employee/delete_employee_by_id/1")
+  }
+
+
+
+  // ================ GET ALL EMPLOYEES========
+
+  public employeeList:any =[];
+
+  // create get all employee in beck end
 
 }
